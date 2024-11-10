9d7218e83e4c6123220b42e58eb570a675ac6bfc
Jimmy Patel <jimmypatel678@yahoo.com>
2024-11-10 13:48:17 -0500

    Remote backend added again

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,15 +1,10 @@
-# terraform {
-#   # Configure the backend for storing Terraform state files remotely
-#   backend "remote" {
-#     # The hostname of the Terraform Cloud instance
-#     hostname = "app.terraform.io"
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "graywolf-tech-hub"
 
-#     # The organization in Terraform Cloud where the workspace is located
-#     organization = "graywolf-tech-hub"
-
-#     workspaces {
-#       # The name of the workspace within the organization
-#       name = "aws-infra"
-#     }
-#   }
-# }
+    workspaces {
+      name = "aws-infra"
+    }
+  }
+}
--- terraform/dev.tfvars
+++ terraform/dev.tfvars
@@ -1,5 +1,7 @@
 # dev.tfvars
-
+hostname             = "app.terraform.io"
+organization         = "graywolf-tech-hub"
+workspace            = "aws-infra"
 aws_region           = "us-east-1"
 vpc_cidr             = "10.0.0.0/16"
 public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
