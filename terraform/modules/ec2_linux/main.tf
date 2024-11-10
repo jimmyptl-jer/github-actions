899e0ecfc353e3a105bdb4c64cdead8eb723830e
Jimmy Patel <jimmypatel678@yahoo.com>
2024-11-10 14:13:12 -0500

    ami data source added

--- terraform/modules/ec2_linux/main.tf
+++ terraform/modules/ec2_linux/main.tf
@@ -1,3 +1,19 @@
+# Create the EC2 instance
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical (Ubuntu) AWS account ID
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "example" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
